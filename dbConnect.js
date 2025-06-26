a621dd03cf9f0bd72bca9bba7509b9b3a9bb288e
jaish ansari <engjaish2004@gmail.com>
2025-06-27 05:22:12 +0530

    commit

--- dbConnect.js
+++ dbConnect.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-    const mongoUri = "mongodb+srv://jaish786:Y12VpQzqxrC9Ycss@cluster0.0ie4hum.mongodb.net/?retryWrites=true&w=majority";
+  const mongoUri =
+    "mongodb+srv://jaish786:Y12VpQzqxrC9Ycss@cluster0.0ie4hum.mongodb.net/?retryWrites=true&w=majority";
 
-    try {
-       const connect = await mongoose.connect( mongoUri ,
-          {           
-            useNewUrlParser: true,
-            //useUnifiedTopology: true,
-           });
+  try {
+    const connect = await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      //useUnifiedTopology: true,
+    });
 
-       console.log(`MongoDB Connected : ${connect.connection.host}`);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-   
+    console.log(`MongoDB Connected : ${connect.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
